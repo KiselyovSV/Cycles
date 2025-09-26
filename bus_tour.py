f3a329e3b0c5c61445324375e265fd93421c24f0
KiselyovSV <KiselyovSV@ya.ru>
2025-09-26 16:12:58 +0300

    changed bus

--- bus_tour.py
+++ bus_tour.py
@@ -1,18 +1,29 @@
+# print("\nBus_tour start")
+# height = 4.37
+# # num_bridges = int(input("Введите количество мостов от 1 до 1000 шт.: "))
+# bridges = input("Через пробел введите высоты мостов ( до 10 метров вкл.):\n ")
+# separated_bridges = bridges.split()
+# counter = 0
+# mes = "No crash"
+# for i in separated_bridges:
+#     counter += 1
+#     if float(i) > 10:
+#         mes = "Недопустимая высота моста!")
+#         break
+#     elif float(i) <= height:
+#         mes = f"Crash {counter}"
+#         break
+# print(mes)
+
 print("\nBus_tour start")
-height = 4.37
-# num_bridges = int(input("Введите количество мостов от 1 до 1000 шт.: "))
-bridges = input("Через пробел введите высоты мостов ( до 10 метров вкл.):\n ")
-separated_bridges = bridges.split()
+bus_height = 4.37
+num_bridges = int(input("Введите количество мостов от 1 до 1000 шт.: "))
 counter = 0
 mes = "No crash"
-for i in separated_bridges:
+for i in range(num_bridges):
+    bridge_heigh = float(input(f"Введите высоту моста № {i} ( до 10 метров вкл.):\n "))
     counter += 1
-    if float(i) > 10:
-        print("Недопустимая высота моста!")
-        mes = ""
-        break
-    elif float(i) <= height:
-        print(f"Crash {counter}")
-        mes = ""
+    if bridge_heigh <= bus_height:
+        mes = f"Crash {counter}"
         break
 print(mes)
