000f9cafaffeb90aa5bc28af08257b115bb132e9
KiselyovSV <KiselyovSV@ya.ru>
2025-09-26 15:09:17 +0300

    changed flowers

--- flowers.py
+++ flowers.py
@@ -1,8 +1,19 @@
+# l = "герань"
+# c = "фикус"
+# r = "крокус"
+# days = int(input("Введите кол-во дней: "))
+# for d in range(days):
+#     c, r = r, c
+#     l, c = c, l
+# print(l, c, r)
+
 l = "герань"
 c = "фикус"
 r = "крокус"
 days = int(input("Введите кол-во дней: "))
-for d in range(days):
-    c, r = r, c
-    l, c = c, l
-print(l, c, r)
+if days % 3 == 0:
+    print(l, c, r)
+elif (days + 2) % 3 == 0:
+    print(r, l, c)
+else:
+    print(c, r, l)
